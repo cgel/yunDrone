280cee0f95bd5773886a57d39e8f9aae23060523
Carles Gelada <carlesgelada@hotmail.com>
2015-04-19 02:46:45 +0200

    A prototype of the library Sensors in implemented (untested). The libraries i2cdevlib have been added to the project.

---
+++ lib/Sensor/sensors.cpp
@@ -0,0 +1,95 @@
+bool Sensors::init()
+{
+	Fastwire::setup(400, true);
+
+	// initialize device
+	mpu.initialize();
+
+	// verify connection
+	bool conectionStatus = mpu.testConnection(); 
+
+	// load and configure the DMP (Digital Motion Processing)
+	devStatus = mpu.dmpInitialize();
+
+	// supply your own gyro offsets here, scaled for min sensitivity
+	mpu.setXGyroOffset(220);
+	mpu.setYGyroOffset(76);
+	mpu.setZGyroOffset(-85);
+	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+	// make sure it worked (devStatus is 0 if so)
+	if (devStatus == 0) {
+		// turn on the DMP, now that it's ready
+		mpu.setDMPEnabled(true);
+
+		// enable Arduino interrupt detection
+		attachInterrupt(0, dmpDataReady, RISING);
+		mpuIntStatus = mpu.getIntStatus();
+
+		// set our DMP Ready flag so the main loop() function knows it's okay to use it
+		dmpReady = true;
+
+		// get expected DMP packet size for later comparison
+		packetSize = mpu.dmpGetFIFOPacketSize();
+		return 0;
+	} else {
+		// ERROR!
+		// 1 = initial memory load failed
+		// 2 = DMP configuration updates failed
+		// (if it's going to break, usually the code will be 1)
+		return 1;
+	}
+}
+
+void Sensors::update()
+{
+	// read the mpu. If there are new reading update rpy[] if there are not return rpy[] whitout update.
+
+	// check if there is an MPU interrupt or extra packet(s) available
+	if (!mpuInterrupt && fifoCount < packetSize) 
+	{
+		//if there is not:
+		return;
+	}
+
+	// reset interrupt flag and get INT_STATUS byte
+	mpuInterrupt = false;
+	mpuIntStatus = mpu.getIntStatus();
+
+	// get current FIFO count
+	fifoCount = mpu.getFIFOCount();
+
+	// check for overflow (this should never happen unless our code is too inefficient)
+	if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+		// reset so we can continue cleanly
+		mpu.resetFIFO();
+
+	// otherwise, check for DMP data ready interrupt (this should happen frequently)
+	} else if (mpuIntStatus & 0x02) {
+		// wait for at least one package, should be a VERY short wait
+		while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+		// there are one ore more packets read all of them and keep the last one
+		while (fifoCount - packetSize > packetSize) 
+		{
+		// read a packet from FIFO
+		mpu.getFIFOBytes(fifoBuffer, packetSize);
+		
+		// track FIFO count here in case there is > 1 packet available
+		fifoCount -= packetSize;
+		} // There is the possibility that while reading the FIFO in the while loop new packets have entered. If it happens we do not read them untill the next update
+		
+
+
+		// display Euler angles in degrees
+		mpu.dmpGetQuaternion(&q, fifoBuffer);
+		mpu.dmpGetGravity(&gravity, &q);
+		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+	}
+}
+
+float[] Sensors::get_ypr()
+{
+	return rpy;
+}
+
---
+++ lib/Sensor/sensors.h
@@ -0,0 +1,48 @@
+#include "I2Cdev.h"
+#include "Scheduler.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+/*
+	Serial.print("ypr\t");
+	Serial.print(ypr[0] * 180/M_PI);
+	Serial.print("\t");
+	Serial.print(ypr[1] * 180/M_PI);
+	Serial.print("\t");
+*/
+
+
+// Interrupt detection routine
+volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() 
+{
+    mpuInterrupt = true;
+}
+
+
+class Sensors: public Process
+{
+	public:
+	bool init();
+
+	void update();
+
+	float[] get_ypr();
+
+	private:
+	MPU6050 mpu;
+
+	// MPU control/status vars
+	bool dmpReady = false;  // set true if DMP init was successful
+	uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+	uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+	uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+	uint16_t fifoCount;     // count of all bytes currently in FIFO
+	uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+	// orientation/motion vars
+	Quaternion q;           // [w, x, y, z]         quaternion container
+	VectorFloat gravity;    // [x, y, z]            gravity vector
+	float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+};
