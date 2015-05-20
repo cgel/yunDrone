12f237764e2c6aa092b60c296cba93554bf62874
carles <carles@localhost.localdomain>
2015-05-20 23:21:39 +0200

    Major modifications to the structure of the scheduler. Undebuged. Untested.

--- libraries/Scheduler/Scheduler.cpp
+++ libraries/Scheduler/Scheduler.cpp
@@ -1,18 +1,8 @@
 #include <Scheduler.h> 
 
-pid_t& pid_t::operator=(const pid_t& p)
-{
-	first = p.first;
-	second = p.second;
-}
-
 // CLASS SCHEDULER
-Scheduler::Scheduler()	
+Scheduler::Scheduler(): 
 {	
-	_50hr_count = 1;
-	_10hr_count = 2;
-	_1hr_count = 3; 
-	_0p1hr_count = 4;
 }
 
 Scheduler::~Scheduler()
@@ -20,71 +10,47 @@ Scheduler::~Scheduler()
 	// this class manages mem in the free store but no need to bother deleting it since this class will never be destroied
 }
 
-pid_t Scheduler::addProcess(Process * pr, const Frequency freq)
+pid_t Scheduler::addProcess(Process& pr, const Frequency freq)
 {
-	processes[freq].push_back(pr);
-	pid_t pid(freq, processes[freq].end() );
-	return pid;
+  ppq->insert(pr);
+
+  //pr is a reference to the process, pid_t is also;
+	return pr;
 }
 
 void Scheduler::killProcess(const pid_t& pid)
 {
 	processes[pid.first].erase(pid.second);
+  // there is a chanse that ppq might return +1 possible values. see multiset doc
+  std::pair<auto, auto> rng = ppq.equal_range(pid);
+  for(;rng.first !=rng.second; ++(rng.first) )
+  {
+    if(rng.first == pid)
+    {
+      ppq.remove(rng.first);
+      println("pid deleted");
+    }
+  }
 }
 
-void Scheduler::update()
+void Scheduler::loop()
 {
+beg:
 	// keep a base "call clock" of a 100hr
 	// Lower frequency calls +++ count of a base 
 	time = millis();	
-	if (time - last_call_time > 10) // 10 milliseconds 
-	{
-		last_call_time = time;
 
-		//this counters are used to track when is time to make the calls
-		//scince they are all multiples of each other if they are all initialized at 50
-		//their calls will be in sync. Which is undesirable because sometimes all will
-		//be called, and sometimes non will. This might degrade the performance.
-		_0p1hr_count++;
-		_1hr_count++;
-		_10hr_count++;
-		_50hr_count++;
+  // high priority process
+  auto iter  = ppq.cbegin();
+  auto hpp  = *iter;
 
+  if( ->pct > time)
+  {
+    hpp->call();
+    ppq.remove(iter);
+    hpp->pct = hpp->tgap + time;
+    ppq.insert(hpp);
+  }
 
-		call_process_list(processes[_100hr]);
-
-		if (_50hr_count == 2)  // 2/100  = 1/50 time to call the 50hr processes
-		{
-			call_process_list(processes[_50hr]);
-			_50hr_count = 0;
-		}
-
-		if (_10hr_count == 10) // 10/100  = 1/10
-		{
-			call_process_list(processes[_10hr]);
-			_10hr_count = 0;
-		} 
-
-		if (_1hr_count == 100) // 100/100  = 1
-		{
-			call_process_list(processes[_1hr]);
-			_1hr_count = 0;
-		} 
-
-		if (_0p1hr_count == 1000) // 1000/100  = 0.1
-		{
-			call_process_list(processes[_0p1hr]);
-			_0p1hr_count = 0;
-		}
-
-		last_call_time = time;
-	}
-}
-
-void Scheduler::call_process_list(std::list<Process*> pr_lst)
-{
-	for (std::list<Process*>::iterator i =  pr_lst.begin(); i != pr_lst.end(); i++)
-	{
-		(*i)->update();	
-	}
+  goto beg;
 }
--- libraries/Scheduler/Scheduler.h
+++ libraries/Scheduler/Scheduler.h
@@ -3,35 +3,33 @@
 
 #include <StandardCplusplus.h>
 #include <list>
+#include <set>
 #include <iterator>
 #include <iterator>
 #include <Arduino.h>
+
+typedef unsigned long millis_t;
 
 enum Frequency { _100hr, _50hr, _10hr, _1hr, _0p1hr};
 
 // derive from this class to create a usable process
 class Process {
-	public:
-	Process() {};
-	//virtual ~Process() = 0;
+  public:
+  Process() {};
+  //virtual ~Process() = 0;
 
-	virtual void update(); //The main function that must be defined when deriving this class.
+  virtual void call(); //The main function that must be defined when deriving this class.
 
-	private:
+  operator>(Process& rhs); // has less priority
+
+  private:
+  millis_t ptc;  // the perfect time to call update
+  millis_t tgap;  // desired time gap between calls ** function of the frequency **
+  millis_t tburst;  // estimated call duration
 };
 
 // pid here means process identifier
-class pid_t {
-	public:
-	pid_t(){};
-
-	pid_t(Frequency f, std::list<Process*>::iterator s): first(f), second(s) {};
-
-	pid_t& operator=(const pid_t&);
-	
-	Frequency first;
-	std::list<Process*>::iterator second;
-};
+typedef Process& pid_t;
 
 class Scheduler {
 	// pid here means process identifier
@@ -39,23 +37,16 @@ class Scheduler {
 	Scheduler();	
 	~Scheduler();
 
-	pid_t addProcess(Process * pr, const Frequency freq);
+	pid_t addProcess(Process& pr, const Frequency freq);
 	void killProcess(const pid_t& pid);
 
-	void update();
+	void loop();
 
 	private:
-	void call_process_list(std::list<Process*>);
-	// processes is an array of vector of iterators (the vectors gup the iteratos of the same freq)
-	std::list<Process*> processes[5];
+	std::multiset<pid_t, std::greater> ppq; // processes priority queue
 
-	short int _0p1hr_count;
-	short int _1hr_count;
-	short int _10hr_count;
-	short int _50hr_count;
-	unsigned long time;
-	unsigned long last_call_time;
+	millis_t time;
+	millis_t last_call_time;
 };
-
 
 #endif
