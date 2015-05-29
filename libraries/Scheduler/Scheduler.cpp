669250cf46aa734c4484356d08591de42548e13c
carles <carles@localhost.localdomain>
2015-05-29 18:12:28 +0200

    Major structural changes in the scheduler. It needs a heap library which currently does not have.

--- libraries/Scheduler/Scheduler.cpp
+++ libraries/Scheduler/Scheduler.cpp
@@ -1,4 +1,4 @@
-#include <Scheduler.h> 
+#include "Scheduler.h"
 
 // ------ CLASS SCHEDULER ----------
 Scheduler::Scheduler() 
@@ -10,27 +10,19 @@ Scheduler::~Scheduler()
 	// this class manages mem in the free store but no need to bother deleting it since this class will never be destroied
 }
 
-pid_t Scheduler::addProcess(Process& pr, const Frequency freq)
+ProcessHandle Scheduler::addProcess(const Process& pr, millis_t m)
 {
-  ppq.insert(&pr);
+  pr.callAgain = true;
+  pr.tgap = m;
 
+  ppq.insert(&pr);
   //pr is a reference to the process, pid_t is also;
 	return &pr;
 }
 
-bool Scheduler::killProcess(pid_t pid)
+bool Scheduler::killProcess(const ProcessHandle h) 
 {
-  // there is a chanse that ppq might return +1 possible values. see multiset doc
-  //std::pair<ppq_iter_t, ppq_iter_t> rng = ppq.equal_range(pid);
-  std::pair<ppq_iter_t, ppq_iter_t> rng = ppq.equal_range(pid);
-  for(;rng.first !=rng.second; ++(rng.first) )
-  {
-    if( *(rng.first) == pid)
-    {
-      ppq.erase(rng.first);
-      cout << "pid deleted" << endl;
-    }
-  }
+  h->callAgain = false;
 }
 
 void Scheduler::loop()
@@ -40,19 +32,21 @@ beg:
 	// Lower frequency calls +++ count of a base 
 	time = millis();	
 
-  // high priority process
-  ppq_iter_t iter  = ppq.begin();
+  // pop the process with the highest priority
+  ProcessHandle h  = ppq.top();
 
-  pid_t hpp  = *iter;
-
-  if( hpp->pct > time)
+  // is it time to call?
+  if( h->pct > time)
   {
-    hpp->call();
-    ppq.erase(iter);
-    hpp->pct = hpp->tgap + time;
-    ppq.insert(hpp);
-  }
+    ppq.pop();
 
+    if(h->callAgain == true)
+    {
+      h->call();
+      h->pct = h->tgap + time;
+      ppq.insert(h);
+    }
+  }
   goto beg;
 }
 
@@ -60,40 +54,13 @@ beg:
 // ------------ CLASS PROCESS ---------
 bool Process::operator>(const Process& rhs) const
 {
-  return pct > rhs.pct;
+  bool b;
+  if(pct > rhs.pct) b = true;
+  else b = (pid > rhs.pid);
+  return b;
 }
 
 bool Process::operator==(const Process& rhs) const
 {
-  return n == rhs.n;
+  return pid == rhs.pid;
 }
-
-
-// ------------- CLASS PID_T -------------------
-pid_t::pid_t() {}
-
-pid_t::pid_t(Process* p)
-{
-  proc = p;
-}
-
-Process& pid_t::operator*() const
-{
-  return *proc;
-}
-
-Process* pid_t::operator->() const
-{
-  return proc;
-}
-
-bool pid_t::operator==(const pid_t& rhs) const
-{
-  return proc->n == rhs.proc->n;
-}
-
-bool pid_t::operator<(const pid_t& rhs) const
-{
-  return proc->pct > rhs.proc->pct;
-}
-
--- libraries/Scheduler/Scheduler.h
+++ libraries/Scheduler/Scheduler.h
@@ -1,21 +1,14 @@
 #ifndef __SCHEDULER_H
 #define __SCHEDULER_H
 
-#include <StandardCplusplus.h>
-//#include <list>
-#include <set>
-//#include <iterator>
 #include <Arduino.h>
+#include "Heap.h"
 
 
-// after debug remove this
-#include <serstream>
 using namespace std;
 
 typedef unsigned long millis_t;
 
-
-enum Frequency { _100hr, _50hr, _10hr, _1hr, _0p1hr};
 
 // derive from this class to create a usable process
 class Process {
@@ -25,6 +18,8 @@ class Process {
 
   virtual void call(); //The main function that must be defined when deriving this class.
 
+  bool priorityCompare( const Process& rhs) const;
+
   bool operator>(const Process& rhs) const; // has less priority
   bool operator==(const Process& rhs) const; // compare is two processes are the same. Done comparing the call function pointers 
 
@@ -32,31 +27,17 @@ class Process {
   millis_t pct;  // the perfect call time update
   millis_t tgap;  // desired time gap between calls ** function of the frequency **
   millis_t tburst;  // estimated call duration
-  short unsigned int n;
-};
 
+  short unsigned int pid;
+
+  bool callAgain;
+};
 
 
 
 // pid here means process identifier
-class  pid_t
-{
-  public:
-  pid_t();
-  pid_t(Process*);
-  Process& operator*() const;
-  Process* operator->() const;
-  bool operator==(const pid_t& rhs) const;
-  bool operator<(const pid_t& rhs) const;
-  Process* proc;
-};
+typedef Process* ProcessHandle;
 
-
-
-typedef std::multiset<pid_t> ppq_t; // processes priority queue
-
-typedef std::multiset<pid_t>::iterator ppq_iter_t; // processes priority queue
-//typedef std::multiset<pid_t>::const_iterator ppq_citer_t; // processes priority queue
 
 class Scheduler {
 	// pid here means process identifier
@@ -64,14 +45,14 @@ class Scheduler {
 	Scheduler();	
 	~Scheduler();
 
-	pid_t addProcess(Process& pr, const Frequency freq);
-	bool killProcess(const pid_t pid);
+	ProcessHandle addProcess(Process& pr, millis_t);
+	bool killProcess(const ProcessHandle handle);
 
 	void loop();
 
 	millis_t time;
 	private:
-  ppq_t ppq; // processes priority queue
+  heap ppq; // processes priority queue
 
 	millis_t last_call_time;
 };
