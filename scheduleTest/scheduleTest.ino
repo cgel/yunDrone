f2b9d1afded8c77362c06128c8b660ae59c378c1
carles <carlesgelada@hotmail.com>
2015-06-06 13:14:06 +0200

    finished scheduler example

--- scheduleTest/scheduleTest.ino
+++ scheduleTest/scheduleTest.ino
@@ -4,41 +4,46 @@
 
 using namespace std;
 // aaaaaaaaaa
-class Helloer: public Process {
+class Talker: public Process {
 	public:
-	Helloer(): msg("") {};
-	Helloer(const char * str): msg(str) {};
-	~Helloer(){};
-
 	//void call() {cout << "Hello my message is:" << msg << endl;};
-	void call() {Serial.print(msg);};
+	void call() {
+  Serial.print(msg);
+    Serial.print(m);
+  };
+
+  void set_msg(const char* str, int _m)
+  {
+    msg = str;
+    m = _m;
+  }
 
 	private:
+        int m;
 	const char *msg;
 };
 
 Scheduler sys;
-Helloer* h1 = new Helloer("a\n");
-Helloer* h2 = new Helloer("-- xx");
-Helloer h3 ("-- -- xx");
+
+Talker procs[2];
 
 void setup(void)
 {
-	Serial.begin(115200);
+	Serial.begin(1200);
 
-	//cout << "Starting Scheduler demo" << endl;
+	Serial.print("Starting Scheduler demo\n");
+delay(500);
+        const char * str = "This is my rate";
+        Serial.print(*str);
+        delay(500);
+  procs[0].set_msg(str, 1000);
+  sys.addProcess(procs[0], 1000);
 
-	sys.addProcess(*h1, 5000);
-	//sys.addProcess(*h2, 3000);
-	//sys.addProcess(h3,  20); //h3 not a pointer
-
-	Serial.begin(115200);
-
-	sys.loop();
+  procs[1].set_msg(str, 2000);
+  sys.addProcess(procs[1], 2000);
 }
 
 void loop(void)
 {
+	sys.loop();
 }
-
-// vim:cin:ai:sts=2 sw=2 ft=cpp
