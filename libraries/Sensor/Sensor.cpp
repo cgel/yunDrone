408f0d1d7f050b94b3df07635c2b917b46fa3ce3
carles <carlesgelada@hotmail.com>
2015-06-07 20:31:45 +0200

    sensor library uses RTIMU now. Not finished yet

--- libraries/Sensor/Sensor.cpp
+++ libraries/Sensor/Sensor.cpp
@@ -1,93 +1,47 @@
 #include <Sensor.h>
 
-Sensor::Sensor(): mpu(), pullMissCount(0), pullEmptyMissCount(0), overflowCount(0), lastBuferReadCount(0), dmpReady(false) {}
-
-bool Sensor::init() {
-	// join I2C bus (I2Cdev library doesn't do this automatically)
-	#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-		Wire.begin();
-		TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-	#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-		Fastwire::setup(400, true);
-	#endif
-
-	// initialize device
-	mpu.initialize();
-
-	// load and configure the DMP
-	devStatus = mpu.dmpInitialize(); 
-
-	// supply your own gyro offsets here, scaled for min sensitivity
-	mpu.setXGyroOffset(220);
-	mpu.setYGyroOffset(76);
-	mpu.setZGyroOffset(-85);
-	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-	// make sure it worked (returns 0 if so)
-	if (devStatus == 0) {
-		// turn on the DMP, now that it's ready
-		mpu.setDMPEnabled(true);
-
-		// set our DMP Ready flag so the main loop() function knows it's okay to use it
-		dmpReady = true;
-
-		// get expected DMP packet size for later comparison
-		packetSize = mpu.dmpGetFIFOPacketSize();
-		return 0;
-	} else {
-		// ERROR!
-		// 1 = initial memory load failed
-		// 2 = DMP configuration updates failed
-		// (if it's going to break, usually the code will be 1)
-		return 1;
-	}
-}
-
-void Sensor::update() {
-	// if programming failed, don't try to do anything
-	if (!dmpReady) return;
-
-	// get current FIFO count
-	fifoCount = mpu.getFIFOCount();
-
-	// check for overflow (this should never happen unless our code is too inefficient)
-	if (fifoCount == 1024) {
-		// reset so we can continue cleanly
-		mpu.resetFIFO();
-		overflowCount += 1;
-	} else if (fifoCount == 0) {
-		pullEmptyMissCount += 1;
-	} else if (fifoCount < packetSize) {
-		pullMissCount += 1;
-	} else { // in this case the fifo has one or more packets
-
-		// if there are more than on packet, will read all of them and keep the newest
-		lastBuferReadCount = 0;
-		while (fifoCount >= packetSize)
-		{
-			// read a packet from FIFO
-			mpu.getFIFOBytes(fifoBuffer, packetSize);
-			lastBuferReadCount += 1;
-
-			// track FIFO count here in case there is > 1 packet available
-			// (this lets us immediately read more without waiting for an interrupt)
-			fifoCount -= packetSize;
-			//fifoCount = mpu.getFIFOCount(); // It is slower than subtracting the packet size, but in case new data comes in it will allow to read it.
-		}
-		// Update Quaternion, Euler angles and Yay Pich Roll angles
-		mpu.dmpGetGyro(ypr_vel, fifoBuffer);
-		mpu.dmpGetQuaternion(&q, fifoBuffer);
-		mpu.dmpGetGravity(&gravity, &q);
-		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-	}
-}
-
-float* Sensor::get_ypr()
+bool Sensor::init()
 {
-	return ypr;
+    int errcode;
+    Wire.begin();
+    imu = RTIMU::createIMU(&settings);                        // create the imu object
+    Serial.print("ArduinoIMU starting using device "); Serial.println(imu->IMUName());
+    if ((errcode = imu->IMUInit()) < 0) {
+        Serial.print("Failed to init IMU: "); Serial.println(errcode);
+    }
+    if (imu->getCalibrationValid())
+        Serial.println("Using compass calibration");
+    else
+        Serial.println("No valid compass calibration data");
+    fusion.setSlerpPower(0.02);
+    fusion.setGyroEnable(true);
+    fusion.setAccelEnable(true);
+    fusion.setCompassEnable(true);
+    return errcode;
 }
 
-int* Sensor::get_ypr_vel()
+void Sensor::call()
+{  
+  while (imu->IMURead()) {                                // get the latest data if ready yet
+    // can the gyrobiasvalid function return false after it was true? dont know so will test it
+    biasValid = imu->IMUGyroBiasValid();
+    if (prevBiasValid == true && biasValid == false)
+    {
+      biasValidChanged = true; // will this flag ever be raised?
+    }
+    prevBiasValid = biasValid;
+
+    fusion.newIMUData(imu->getGyro(), imu->getAccel(), imu->getCompass(), imu->getTimestamp());
+  }
+}
+
+void Sensor::print()
+{  
+    RTMath::displayRollPitchYaw("Pose:", (RTVector3&)fusion.getFusionPose()); // fused output
+    Serial.println();
+}
+
+bool Sensor::ready() // the imu might not be ready for a few seconds after init
 {
-	return ypr_vel;
+  return biasValid;
 }
--- libraries/Sensor/Sensor.h
+++ libraries/Sensor/Sensor.h
@@ -2,54 +2,36 @@
 #define __SENSOR__MPU_H
 
 #include "Scheduler.h"
-
-#include "MPU6050_6Axis_MotionApps20.h"
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
+#include <Wire.h>
 #include "I2Cdev.h"
+#include "RTIMUSettings.h"
+#include "RTIMU.h"
+#include "RTFusionRTQF.h" 
+#include "CalLib.h"
+#include <EEPROM.h>
 
-class Sensor : public Process {
+
+class Sensor: public Process{
 public:
-	Sensor();
-
 	bool init();
-
-	void update();
-
+	void call();
+  bool ready(); // the imu might not be ready for a few seconds after init
 	float* get_ypr();
 	int* get_ypr_vel();
 
-	// some usefull variables for debugging
-	int pullMissCount;
-	int pullEmptyMissCount;
-	int overflowCount;
-	int lastBuferReadCount;
-
+  bool biasValidChanged;
+  void print();
 private:
-	MPU6050 mpu;
+  RTIMU *imu;                                           // the IMU object
+  RTFusionRTQF fusion;                                  // the fusion object
+  RTIMUSettings settings;                               // the settings object
 
-	// MPU control/status vars
-	bool dmpReady;  // set true if DMP init was successful
-	uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-	uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-	uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-	uint16_t fifoCount;     // count of all bytes currently in FIFO
-	uint8_t fifoBuffer[64]; // FIFO storage buffer
+  //
+  bool biasValid;
+  bool prevBiasValid;
 
-	// orientation/motion vars
-	Quaternion q;           // [w, x, y, z]         quaternion container
-	VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-	VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-	VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-	VectorFloat gravity;    // [x, y, z]            gravity vector
-	float euler[3];         // [psi, theta, phi]    Euler angle container
 	float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 	int ypr_vel[3];          // [yaw, pitch, roll]   yaw/pitch/roll
 };
-
 
 #endif
